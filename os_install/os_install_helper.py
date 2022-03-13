743efa6bca5fdfe0ecccc5f0e0edadf4ba1901d4
Adam Imdieke <adam.imdie022@gmail.com>
2022-03-12 19:02:50 -0600

    Replace os_clone with a GUI version

---
+++ os_install/os_install_helper.py
@@ -0,0 +1,54 @@
+#!/bin/env python3
+
+import os
+from os.path import exists
+import json
+
+def get_block_devices(normalization):
+    os.system('lsblk --json --tree > ./lsblk.tmp')
+
+    f=open('./lsblk.tmp')
+    drives=json.load(f)
+    f.close()
+    
+    normal={'blockdevices': []}
+    if(exists(normalization)):
+        f=open(normalization)
+        normal=json.load(f)
+        f.close
+
+    
+    ret=[]
+    for drive in drives['blockdevices']:
+        if(drive not in normal['blockdevices']):
+            ret.append(drive)
+    return ret
+
+def get_iso_roms(iso_dir):
+    roms=os.listdir(iso_dir)
+    iso_roms=[]
+    for romf in roms:
+        if(os.path.isdir(os.path.join(iso_dir,romf))):
+            iso_roms.append({'path': os.path.join(iso_dir, romf), 'name': romf})
+    
+    valid_roms=[]
+    for rom in iso_roms:
+        files=os.listdir(rom['path'])
+        
+        if(rom['name']+'.conf' in files):
+            with open(rom['path']+'/'+rom['name']+'.conf') as namef:
+                rom['about']=namef.readline().strip()
+        else:
+            rom['about']=rom['name']
+
+        if(rom['name']+'.iso' in files):
+            if(rom['name'] in files):
+                valid_roms.append(rom)
+            else:
+                print('Missing executable for: '+rom['name'])
+        else:
+            print('Missing .iso for: '+rom['name'])
+    return valid_roms
+
+
+
