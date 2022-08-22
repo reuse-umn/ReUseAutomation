0898febb332d4e65b6c3a372e36e665ddb33ba00
Adam Imdieke <adam.imdie022@gmail.com>
2022-08-22 15:10:40 -0500

    Modified TDX entery

---
+++ TDX_entery/tdx_body_top.py
@@ -0,0 +1,5 @@
+delay = 1
+
+keyboard.send_keys("\t\t\t\t\t")
+keyboard.send_keys("Stock")
+time.sleep(delay)
--- TDX_entery/tdx_repair.py
+++ TDX_entery/tdx_repair.py
@@ -1,14 +1,3 @@
-delay = 1
-keyboard.send_keys("\t\t\t\t\t")
-keyboard.send_keys("Stock")
-time.sleep(delay)
+engine.run_script("tdx_body_top")
 keyboard.send_keys("\trepair")
-time.sleep(delay)
-keyboard.send_keys("\t\t\t\t\t ")
-keyboard.send_keys("\t\t10293")
-time.sleep(delay*6)
-keyboard.send_keys("\t\t\t\tahcware")
-time.sleep(delay*5)
-for i in range(13):
-    keyboard.send_keys("\t")
-    time.sleep(delay*.25)
+engine.run_script("tdx_body_bottom")
