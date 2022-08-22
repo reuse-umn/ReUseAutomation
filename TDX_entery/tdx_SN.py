621b388dd765f309e78050e99a960a8372764bf6
Adam Imdieke <adam.imdie022@gmail.com>
2022-08-22 16:09:22 -0500

    More bug fixing

--- TDX_entery/tdx_SN.py
+++ TDX_entery/tdx_SN.py
@@ -1,2 +1,6 @@
 for i in range(30):
     keyboard.send_keys("<shift>+<tab>")
+
+keyboard.send_keys("<shift>+<tab>")
+keyboard.send_keys("<page_up>")
+keyboard.send_keys("<tab>")
--- TDX_entery/tdx_ST.py
+++ TDX_entery/tdx_ST.py
@@ -1,8 +1,13 @@
 for i in range(28):
     keyboard.send_keys("<shift>+<tab>")
-keyboard.wait_for_keypress("<enter>", modifiers=[], timeOut=30.0)
-for i in range(7):
+
+keyboard.send_keys("<shift>+<tab>")
+keyboard.send_keys("<page_up>")
+keyboard.send_keys("<tab>")
+
+keyboard.wait_for_keypress("<tab>", modifiers=[], timeOut=30.0)
+for i in range(5):
     keyboard.send_keys("<shift>+<tab>")
-keyboard.wait_for_keypress("<enter>", modifiers=[], timeOut=30.0)
-for i in range(3):
+keyboard.wait_for_keypress("<tab>", modifiers=[], timeOut=30.0)
+for i in range(4):
     keyboard.send_keys("<shift>+<tab>")
